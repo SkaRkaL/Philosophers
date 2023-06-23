2ea1cf2159008010f854bf198777815b729847c6
Saad Karkal <karkalsaad@gmail.com>
2023-06-23 05:20:08 +0100

    header file

--- philo.h
+++ philo.h
@@ -9,21 +9,21 @@
 
 typedef struct s_philo
 {
-	int				id;
-	int				done;
-	int				n_philo;
-	unsigned long long			last_meal;
-	pthread_mutex_t	fork;
-	pthread_mutex_t	*print;
-	unsigned long long			n_meals;
-	int							check;
-	unsigned long long			max_tto_eat;
-	unsigned long long			time_to_die;
-	unsigned long long			time_to_eat;
-	unsigned long long			time_to_sleep;
-	pthread_t		philos;
-	struct s_philo	*next;
-} 		t_philo;
+	int id;
+	int done;
+	int n_philo;
+	unsigned long long last_meal;
+	pthread_mutex_t fork;
+	pthread_mutex_t *print;
+	unsigned long long n_meals;
+	int check;
+	unsigned long long max_tto_eat;
+	unsigned long long time_to_die;
+	unsigned long long time_to_eat;
+	unsigned long long time_to_sleep;
+	pthread_t philos;
+	struct s_philo *next;
+} t_philo;
 
 // ---------------- LIST -----------------
 
@@ -34,9 +34,9 @@ void ft_lstadd_back(t_philo **lst, t_philo *new);
 
 // ---------------------------------------
 
-unsigned long	in_time(void);
-int	my_sleep(unsigned int t_todo);
-int	all_ate(t_philo *philo);
+unsigned long in_time(void);
+int my_sleep(unsigned int t_todo);
+int check_eating(t_philo *philo);
 
 // ---------------------------------------
 
