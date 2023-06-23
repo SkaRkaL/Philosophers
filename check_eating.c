c5344385d2a4f3842799fef558dde4f12ba6e47c
Saad Karkal <karkalsaad@gmail.com>
2023-06-23 05:18:35 +0100

    check philos eating

---
+++ check_eating.c
@@ -0,0 +1,20 @@
+#include "philo.h"
+
+int	all_ate(t_philo *philo)
+{
+	int i;
+	int ate;
+
+	i = 0;
+	ate = 0;
+	while (i < philo->n_philo)
+	{
+		if (philo->n_meals >= philo->max_tto_eat)
+			ate++;
+		philo = philo->next;
+		i++;
+	}
+	if (ate == philo->n_philo)
+		return (1);
+	return (0);
+}
